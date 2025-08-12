d7e85eb12ddc1ec783e678a7161f107517004dac
Bas Milius <basmilius@hotmail.com>
2025-08-12 15:42:48 +0200

    feat(http): add default constraints.

---
+++ src/Constraint/IsDateTime.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Raxos\DateTime\Constraint;
+
+use Attribute;
+use Raxos\DateTime\DateTime;
+use Raxos\Http\Validate\Contract\ConstraintAttributeInterface;
+use Raxos\Http\Validate\Error\HttpConstraintException;
+use ReflectionProperty;
+use function is_string;
+use function preg_match;
+
+/**
+ * Class IsDateTime
+ *
+ * @author Bas Milius <bas@mili.us>
+ * @package Raxos\DateTime\Constraint
+ * @since 2.0.0
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final readonly class IsDateTime implements ConstraintAttributeInterface
+{
+
+    public const string REGEXP = /* @lang PhpRegExp */
+        '/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}\+\d{2}:\d{2}/';
+
+    /**
+     * {@inheritdoc}
+     * @author Bas Milius <bas@mili.us>
+     * @since 2.0.0
+     */
+    public function check(ReflectionProperty $property, mixed $value): DateTime
+    {
+        if (!is_string($value) || !preg_match(self::REGEXP, $value)) {
+            throw HttpConstraintException::of(
+                'is_datetime',
+                'Invalid date time.'
+            );
+        }
+
+        return DateTime::parse($value)->setTimezone('UTC');
+    }
+
+}
